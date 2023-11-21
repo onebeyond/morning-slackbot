5437422ec74868d97109e70b3bc28021f5864ef9
acalvom <andreac.3108@gmail.com>
2023-11-21 11:43:06 +0100

    feat: create model for weather information

---
+++ src/models/Weather.ts
@@ -0,0 +1,12 @@
+export interface Weather {
+  code: string;
+  city: string;
+  country: string;
+  currentTemp: string;
+  condition: number;
+  maxTemp: string;
+  minTemp: string;
+  rain: string;
+  sunrise: string;
+  sunset: string;
+}
