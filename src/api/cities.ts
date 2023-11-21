f57b21c780b8742b962a8d67e4dc59c693c5ba8a
acalvom <andreac.3108@gmail.com>
2023-11-21 14:14:54 +0100

    fix: endpoint type

--- src/api/cities.ts
+++ src/api/cities.ts
@@ -1,12 +1,11 @@
 import { createClient } from '../lib/http-client';
-import { Weather } from '../models';
 
 const http = createClient({
   baseURL: process.env.API_URL,
   withCredentials: false,
 });
 
-export const getWeatherByCity = async (city: string): Promise<Weather> => {
+export const getWeatherByCity = async (city: string): Promise<any> => {
   try {
     return await http.get(`/forecast.json?q=${city}&days=1&key=${process.env.API_KEY}`);
   } catch (error) {
