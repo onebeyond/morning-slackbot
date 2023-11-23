f4d2a28049fe42d4e49ea5e831e9b7203cb81724
acalvom <andreac.3108@gmail.com>
2023-11-23 12:51:40 +0100

    feat: add more keys to weather model

--- src/models/Weather.ts
+++ src/models/Weather.ts
@@ -1,7 +1,9 @@
 export interface Weather {
   code: string;
   city: string;
+  cityEmoji: string;
   country: string;
+  countryEmoji: string;
   currentTemp: string;
   condition: number;
   maxTemp: string;
--- src/utils/weatherMapper.ts
+++ src/utils/weatherMapper.ts
@@ -1,10 +1,15 @@
+import { CountryIcons, CityEmojis } from '../constants';
 import { Weather } from '../models';
+
+import { randomEmoji } from './randomEmoji';
 
 export const weatherMapper = (weather: any, cityCode: string) => {
   const mappedWeather: Weather = {
     code: cityCode,
     city: weather.location.name,
+    cityEmoji: randomEmoji(CityEmojis[cityCode]),
     country: weather.location.country,
+    countryEmoji: CountryIcons[weather.location.country],
     currentTemp: Math.round(weather.current.temp_c) + 'ºC',
     condition: weather.forecast.forecastday[0].day.condition.code,
     maxTemp: Math.round(weather.forecast.forecastday[0].day.maxtemp_c) + 'ºC',
@@ -13,5 +18,6 @@ export const weatherMapper = (weather: any, cityCode: string) => {
     sunrise: weather.forecast.forecastday[0].astro.sunrise,
     sunset: weather.forecast.forecastday[0].astro.sunset,
   };
+  console.log(mappedWeather);
   return mappedWeather;
 };
